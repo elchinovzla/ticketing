f7287b2c0720ee6a2cf0ae2ea3fce89f95a1d7db
elchinovzla <sonnyng.vzla@gmail.com>
2023-06-26 15:54:12 -0600

    add jwt token into sign up

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
@@ -9,7 +10,14 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -19,6 +27,10 @@ app.use(signoutRouter);
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY should be define");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
   } catch (err) {
--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -15,16 +15,28 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttributes): UserDoc;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret.password;
+        delete ret.__v;
+        delete ret._id;
+      },
+    },
+  }
+);
 userSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
     const hashed = await Password.toHash(this.get("password"));
