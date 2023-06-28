867bc4dbdbd224ca3dbd4ee0f34762106b08dc36
elchinovzla <sonnyng.vzla@gmail.com>
2023-06-28 15:17:12 -0600

    add auth service tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,30 +1,5 @@
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import { errorHandler } from "./middlewares/error-handlers";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
