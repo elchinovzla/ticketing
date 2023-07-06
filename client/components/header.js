aa8919950368fd9a7b7018d83d28ef065942c1b8
elchinovzla <sonnyng.vzla@gmail.com>
2023-07-06 17:19:28 -0600

    add in client sign in and signout + header

---
+++ client/components/header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign up", href: "/auth/signup" },
+    !currentUser && { label: "Sign in", href: "auth/signin" },
+    currentUser && { label: "Sign out", href: "auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <p className="nav-link">{label}</p>
+          </Link>
+        </li>
+      );
+    });
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/">
+        <p className="navbar-brand">sTickets</p>
+      </Link>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <Header currentUser={pageProps.currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const response = await client.get("/api/users/currentuser").catch((error) => {
+    console.log(error.message);
+  });
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    currentUser: response?.data.currentUser ?? {},
+  };
+};
+
+export default AppComponent;
