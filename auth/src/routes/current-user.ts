0f7541e99dbc89f95b87b57c566065ae7b7926cf
elchinovzla <sonnyng.vzla@gmail.com>
2023-07-12 15:54:04 -0600

    consume auth code from npm elchinvzla-common library

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
+import { currentUser } from "@elchinovzla-common/auth";
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
