a381db449a3a66705cfba5544592d6e5b6854699
elchinovzla <sonnyng.vzla@gmail.com>
2023-10-12 16:12:58 -0600

    create expiration service + publish expiration:complete after 1 min

---
+++ expiration/src/index.ts
@@ -0,0 +1,42 @@
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  if (!process.env.NATS_CLUSTER) {
+    throw new Error('NATS cluster should be define');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS client id should be define');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS url should be define');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => {
+      console.log('SIGINT process shutting down process');
+      natsWrapper.client.close();
+    });
+    process.on('SIGTERM', () => {
+      console.log('SIGTERM process shutting down process');
+      natsWrapper.client.close();
+    });
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
