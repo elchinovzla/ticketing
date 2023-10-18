4d0a4cd4d4cce59060e9efe939d35a2559c65355
elchinovzla <sonnyng.vzla@gmail.com>
2023-10-18 09:42:38 -0600

    update client app to send current user through the components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,24 +1,29 @@
-import "bootstrap/dist/css/bootstrap.css";
-import buildClient from "../api/build-client";
-import Header from "../components/header";
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <Header currentUser={pageProps.currentUser} />
-      <Component {...pageProps} />
+      <Header currentUser={currentUser} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
   );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
-  const response = await client.get("/api/users/currentuser").catch((error) => {
+  const response = await client.get('/api/users/currentuser').catch((error) => {
     console.log(error.message);
   });
+
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   return {
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,9 @@
-import buildClient from "../api/build-client";
-
 const LandingPage = ({ currentUser }) => {
-  return <h1>You are {currentUser ? "" : "not"} signed in</h1>;
+  return <h1>You are {currentUser ? '' : 'not'} signed in</h1>;
 };
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const response = await client.get("/api/users/currentuser").catch((error) => {
-    console.log(error.message);
-  });
-
-  return response?.data ?? {};
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
