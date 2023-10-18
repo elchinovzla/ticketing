f49081366fba10c18e509d5a49d24f84662c14c7
elchinovzla <sonnyng.vzla@gmail.com>
2023-10-18 17:32:59 -0600

    update ingress base url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default ({ req }) => {
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     //we are calling from the server
     return axios.create({
-      baseURL: "http://ingress-nginx.ingress-nginx-controller",
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx',
       headers: req.headers,
     });
   } else {
     //we are calling from the brower/client
     return axios.create({
-      baseURL: "/",
+      baseURL: '/',
     });
   }
 };
